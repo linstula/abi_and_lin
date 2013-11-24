6db5464e555658cb794f936b0d4df2a1cd4575e6
Lin Reid <linreid@gmail.com>
2013-11-24 17:08:23 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AbiAndLin::Application.config.secret_key_base = 'b21951794da7fd30502493fd66d7238b4a728b6898c39efc6c73f86481d75fc32af59dade4b7a289b808c87d92b244859a3e1ac7254354f660633213e369a74b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AbiAndLin::Application.config.session_store :cookie_store, key: '_abi_and_lin_session'
