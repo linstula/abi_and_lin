f8883d79d95a8c8205a382d1c8c915632f4d09ce
Lin Reid <linreid@gmail.com>
2013-11-24 17:16:39 -0500

    bootstrap ember

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+
+//= require jquery
+//= require ember-app
 //= require_tree .
---
+++ app/assets/javascripts/ember-app.js
@@ -0,0 +1,16 @@
+//= require ember-env
+//= require ember-appkit
+//= require ember-data
+//= require_self
+//= require store
+//= require router
+//= require_tree ./mixins
+//= require_tree ./models
+//= require_tree ./templates
+//= require_tree ./controllers
+//= require_tree ./components
+//= require_tree ./routes
+//= require_tree ./views
+//= require_tree ./helpers
+
+window.App = require('app').default.create();
